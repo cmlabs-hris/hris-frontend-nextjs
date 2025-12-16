12619d4eba8e88fc4038818f00ac34bad43b28b9
Naxumi <muhammad.hafizhalfurqon@gmail.com>
2025-12-16 17:50:00 +0700

    feat: update next.config.ts to include image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig = {
-
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8080',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
   eslint: {
-
     ignoreDuringBuilds: true,
   },
 };
